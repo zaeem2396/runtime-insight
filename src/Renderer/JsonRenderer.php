f9374cea662010c084f3f41e55d56848b3996191
zaeem2396 <zaeemansari87@gmail.com>
2026-02-02 23:53:16 +0530

    feat(fix): fix code style using PHP-CS-FIXER

--- src/Renderer/JsonRenderer.php
+++ src/Renderer/JsonRenderer.php
@@ -9,6 +9,10 @@ use ClarityPHP\RuntimeInsight\DTO\Explanation;
 
 use function json_encode;
 
+use const JSON_PRETTY_PRINT;
+use const JSON_THROW_ON_ERROR;
+use const JSON_UNESCAPED_SLASHES;
+
 /**
  * Renders an explanation as JSON.
  */
@@ -17,7 +21,7 @@ final class JsonRenderer implements RendererInterface
     public function render(Explanation $explanation): string
     {
         $data = $explanation->toArray();
-        $encoded = json_encode($data, \JSON_THROW_ON_ERROR | \JSON_UNESCAPED_SLASHES | \JSON_PRETTY_PRINT);
+        $encoded = json_encode($data, JSON_THROW_ON_ERROR | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
 
         return $encoded;
     }
