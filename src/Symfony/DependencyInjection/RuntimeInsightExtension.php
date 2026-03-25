c8666f5e77937da3cbc1e478d7108552caefe471
zaeem2396 <zaeemansari87@gmail.com>
2026-03-26 00:51:33 +0530

    Symfony DI: load services.yaml from package root config/

--- src/Symfony/DependencyInjection/RuntimeInsightExtension.php
+++ src/Symfony/DependencyInjection/RuntimeInsightExtension.php
@@ -33,7 +33,7 @@ final class RuntimeInsightExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container): void
     {
-        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../../config'));
+        $loader = new YamlFileLoader($container, new FileLocator(\dirname(__DIR__, 3) . '/config'));
         $loader->load('services.yaml');
 
         $configuration = new Configuration();
