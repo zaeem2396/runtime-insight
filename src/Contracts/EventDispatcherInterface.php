cb561561f943817098720bb59c761dc00f611065
zaeem2396 <zaeemansari87@gmail.com>
2026-03-26 00:51:33 +0530

    EventDispatcherInterface: add addListener for extensibility

--- src/Contracts/EventDispatcherInterface.php
+++ src/Contracts/EventDispatcherInterface.php
@@ -10,6 +10,13 @@ namespace ClarityPHP\RuntimeInsight\Contracts;
 interface EventDispatcherInterface
 {
     /**
+     * Register a listener for an event class. Invoked when dispatch() receives that class.
+     *
+     * @param callable(object): void $listener
+     */
+    public function addListener(string $eventClass, callable $listener): void;
+
+    /**
      * Dispatch an event to all registered listeners for that event class.
      */
     public function dispatch(object $event): void;
